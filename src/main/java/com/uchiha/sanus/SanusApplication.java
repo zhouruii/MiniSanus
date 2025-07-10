d9c6c4ae636b72cf96920086e3776e7e60341578
Rui.Zhou <zhourui203316@163.com>
2025-07-10 20:22:45 +0800

    初始化

---
+++ src/main/java/com/uchiha/sanus/SanusApplication.java
@@ -0,0 +1,13 @@
+package com.uchiha.sanus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uchiha/sanus/SanusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uchiha.sanus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
