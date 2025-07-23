0dfce5c4b282fe5cd30ac40889b520aeb72fcdad
Rui.Zhou <zhourui203316@163.com>
2025-07-23 22:59:16 +0800

    更新智能体提示词

--- src/main/java/com/uchiha/sanus/agent/MiniSanus.java
+++ src/main/java/com/uchiha/sanus/agent/MiniSanus.java
@@ -13,15 +13,15 @@ public class MiniSanus extends ToolCallAgent {
         super(allTools);
         this.setName("MiniSanus");
         String SYSTEM_PROMPT = """  
-                You are MiniSanus, an all-capable AI assistant, aimed at solving any task presented by the user.  
-                You have various tools at your disposal that you can call upon to efficiently complete complex requests.  
+                You are MiniSanus, an all-capable AI assistant, aimed at solving scientific research tasks presented by the user.
+                You have various tools at your disposal that you can call upon to efficiently complete complex requests.
                 """;
         this.setSystemPrompt(SYSTEM_PROMPT);
         String NEXT_STEP_PROMPT = """  
-                Based on user needs, proactively select the most appropriate tool or combination of tools.  
-                For complex tasks, you can break down the problem and use different tools step by step to solve it.  
-                After using each tool, clearly explain the execution results and suggest the next steps.  
-                If you want to stop the interaction at any point, use the `terminate` tool/function call.  
+                Based on user needs, proactively select the most appropriate tool or combination of tools.
+                For complex tasks, you can break down the problem and use different tools step by step to solve it.
+                After using each tool, clearly explain the execution results and suggest the next steps.
+                If you want to stop the interaction at any point, use the `terminate` tool/function call.
                 """;
         this.setNextStepPrompt(NEXT_STEP_PROMPT);
         this.setMaxSteps(10);
